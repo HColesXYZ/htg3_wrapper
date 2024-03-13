1dc9f699ac4257d2f3a2a8505dd017a6eb1da60b
Hunter Coles <hunter.coles@xyzreality.com>
2024-03-13 12:42:57 -0600

    Init Commit

---
+++ output/README.md
@@ -0,0 +1,5 @@
+# Output Data Folder
+
+> This is a **placeholder**
+
+This is were the output runs will go
---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include "ros/ros.h"
+
+bool getROSParam(const ros::NodeHandle& nh, const std::string& node_name, const std::string& param_name, std::string& param_value) {
+
+    std::string param_name_full = "/" + node_name + "/" + param_name;
+
+    if (!nh.getParam(param_name_full, param_value)) {
+        ROS_ERROR("Failed to get parameter '%s'", param_name.c_str());
+        return false;
+    }
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    // Setup ROS Node and read config file
+    ros::init(argc, argv, "htg3_wrapper");
+    ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
+
+    ros::NodeHandle nh;
+    std::string node_name = ros::this_node::getName();
+    std::string config_param, data_param;
+
+    if (!getROSParam(nh, node_name, "data_param", data_param)) return 1;
+    if (!getROSParam(nh, node_name, "config_param", config_param)) return 1;
+    ROS_INFO("Config Param: %s", config_param.c_str());
+    ROS_INFO("Data Param: %s", data_param.c_str());
+
+    //run_HTG3(config_param,data_param);
+    return 0;
+}
